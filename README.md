3bf85a343e5807c2a0a9ceaa5efd39abad9b98d8
Tanya <pinki_00@mail.ru>
2023-12-19 22:01:06 +0300

    feat: add container, header and switcher components

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# TypeScript курс. Проект GitHub user finder
+# GitHub user finder
--- templates/default/TemplateName.tsx
+++ templates/default/TemplateName.tsx
@@ -1,9 +1,8 @@
+import React from 'react';
 import styles from './TemplateName.module.scss';
 
-interface TemplateNameProps { }
+interface TemplateNameProps {}
 
-export const TemplateName = ({ }: TemplateNameProps) => (
-  <div className={styles.TemplateName} data-testid="TemplateName">
-    TemplateName Component
-  </div>
+export const TemplateName = ({}: TemplateNameProps) => (
+  <div className={styles.templateName}>TemplateName Component</div>
 );
